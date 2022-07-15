abcd6c2772b83cfb35a6f0086778c4da26716aa0
Nasimuddin Sk <snasim251@email.com>
2022-07-15 16:04:18 +0300

    day2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/foundation/key.dart';
+
+class HomePage extends StatelessWidget {
+  var days = 30;
+  final String name = "Nasimuddin Sk";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
